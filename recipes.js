a16207f0ea39e2e1b7f5f52d913aef8f69c98c31
annacv <21217131+annacv@users.noreply.github.com>
2019-07-18 12:08:08 +0200

    [L] ddbb import, logic, methods

--- recipes.js
+++ recipes.js
@@ -1,6 +1,9 @@
+'use strict';
+
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+
 const data = require('./data.js');
+const Recipe = require('./models/Recipe.js')
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
@@ -8,3 +11,36 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+/* here the method related to the model, defines model's props'values == it creates one document in database */
+const createOneRecipe = async () => {
+  try {
+    const response = await Recipe.create({
+      title: 'Brocoli con patatas',
+      level: 'UltraPro Chef',
+      ingredients: ['brocoli', 'patatas', 'ajo', 'aceite', 'sal'],
+      cuisine: 'veggie',
+      dishType: 'Dish',
+      duration: 30,
+      creator: 'Anna'
+    })
+    console.log(response.title);
+  }
+  catch(error) {
+    console.log(error);
+  }
+}
+
+// insert list of recipes from data.js 
+const addManyRecipes = async (data) => {
+  try {
+    const response = await Recipe.insertMany(data);
+    response.forEach((recipe) => console.log(recipe.title))
+  }
+  catch(error) {
+    console.log(error);
+  }
+}
+
+createOneRecipe();
+addManyRecipes(data);
